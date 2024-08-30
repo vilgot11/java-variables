bdd54319e848ad51d40ffa67fff7ae41febd2f8c
vilgot11 <vilgot.ahlfors@gmail.com>
2024-08-30 01:45:28 -0700

    Add files via upload

---
+++ Javaprov/Uppgift1/Uppgift1JavaProv.java
@@ -0,0 +1,22 @@
+
+public class Uppgift1JavaProv {
+     
+    public static void main(String args[]) {
+        int Num1 = 5;
+        
+        int Num2 = 10;
+        
+        int Num3 = 25;
+        
+        int Num4 = 75;
+        
+        int Num5 = 125;
+        
+        System.out.println(Num1 + Num5);
+        System.out.println(Num4 - Num2);
+        System.out.println(Num3 / Num1);
+        System.out.println(Num5 * Num4);
+                
+                
+    }
+}
---
+++ Javaprov/Uppgift3/Uppgift3JavaProv.java
@@ -0,0 +1,10 @@
+
+public class Uppgift3JavaProv {
+
+  
+    public static void main(String args[]) {
+        for (int i =0; i <= 4; i++){
+        System.out.println(i);
+    }
+    }
+}
