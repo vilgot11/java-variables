054a13bcaba8d64e70109bd4c2ca77064d56dc0e
vilgot11 <vilgot.ahlfors@gmail.com>
2024-09-22 23:37:29 -0700

    Add files via upload

---
+++ java uppgifter/JavaUppgift2.java
@@ -0,0 +1,19 @@
+
+public class JavaUppgift2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+    int Num1 = 10;
+    System.out.println(Num1);
+    
+    int Num2 = 5;
+    System.out.println(Num2);
+    
+    System.out.println(Num1 + Num2);
+    System.out.println(Num1 - Num2);
+    System.out.println(Num1 * Num2);
+    System.out.println(Num1 / Num2);
+    }
+}
