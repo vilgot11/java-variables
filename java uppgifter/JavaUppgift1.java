fb2e137ab735ab481b64ac165c9baf0c962eba8a
vilgot11 <vilgot.ahlfors@gmail.com>
2024-09-22 23:32:57 -0700

    Add files via upload

---
+++ java uppgifter/JavaUppgift1.java
@@ -0,0 +1,31 @@
+
+ 
+public class JavaUppgift1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        String firstName = "vilgot";
+        String lastName = "Ahlfors";
+        
+        String fullName = firstName + " " + lastName;
+        System.out.println(fullName);
+        
+        int Age = 17;
+        System.out.println(Age);
+        
+        
+        double height = 170;
+        System.out.print(height);
+        
+        String txt = "cm";
+        System.out.println(txt);
+        
+        
+        boolean Isastudent = true;
+        System.out.println(Isastudent);
+        
+        
+    }
+}
