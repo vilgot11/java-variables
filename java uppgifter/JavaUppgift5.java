2f55639794c7e116f9dbea22384eb2e2539153b7
vilgot11 <vilgot.ahlfors@gmail.com>
2024-09-22 23:42:55 -0700

    Add files via upload

---
+++ java uppgifter/JavaUppgift5.java
@@ -0,0 +1,22 @@
+
+public class JavaUppgift5 {
+
+    
+    public static void main(String args[]) {
+    int score = 27;
+         if (score >= 90){
+             System.out.println("5");
+         }else if (score >= 80){
+             System.out.println("4");
+         }else if (score >= 70){
+             System.out.println("3");
+         }else if (score >= 60){
+              System.out.println("2");
+         }else if (score >= 25){
+             System.out.println("1");
+         }else if (score >= 24)
+         System.out.println("0");
+    }
+    }
+             
+         
