19879b1ae7d7a4ae89145b99b0ce09a011554e68
vilgot11 <vilgot.ahlfors@gmail.com>
2024-09-22 23:39:54 -0700

    Add files via upload

---
+++ java uppgifter/JavaUppgift4.java
@@ -0,0 +1,16 @@
+
+ 
+public class JavaUppgift4 {
+
+    
+    public static void main(String args[]) {
+      int number = 10;
+      
+      if(number % 2 == 0){
+          System.out.println(number + "is even");
+      }else{
+          System.out.println(number + "is ood");
+      }
+   
+    }
+}
