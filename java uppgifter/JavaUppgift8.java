a4034a82a911bff6bcd4cf706dd9a3610c81f938
vilgot11 <vilgot.ahlfors@gmail.com>
2024-09-25 12:01:57 +0300

    Add files via upload

---
+++ java uppgifter/JavaUppgift8.java
@@ -0,0 +1,14 @@
+
+public class JavaUppgift8 {
+
+   
+    public static void main(String args[]) {
+        for (int i =1; i < 31; i++) {
+            if (i == 12) {
+                break;
+            }
+            System.out.println(i);
+        }
+        System.out.println("Loop terminated early due to break statment");
+    }
+}
