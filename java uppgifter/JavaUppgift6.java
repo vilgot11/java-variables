733654a77c18b0790f43bf50762b803da3d1c6cc
vilgot11 <vilgot.ahlfors@gmail.com>
2024-09-25 13:17:27 +0300

    Add files via upload

---
+++ java uppgifter/JavaUppgift6.java
@@ -0,0 +1,12 @@
+
+public class JavaUppgift6 {
+
+    
+    public static void main(String args[]) {
+        if(false){
+            System.out.println("on");
+        }else{
+           System.out.println("off");
+        }
+    }
+}
