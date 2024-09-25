a957ac9b230b5a2d3d516ccfe458a505bd259a21
vilgot11 <vilgot.ahlfors@gmail.com>
2024-09-25 13:03:00 +0300

    Add files via upload

---
+++ java uppgifter/JavaUppgift3.java
@@ -0,0 +1,36 @@
+
+public class JavaUppgift3 {
+
+
+    public static void main(String args[]) {
+       int i = 8;
+       double I = i;
+          System.out.println(I);
+       
+       
+       float f = 0.1F;
+       double d = f;
+          System.out.println(d);
+        
+       /*
+       double myDouble =5.25d;
+       int myint = (int)myDouble;
+       
+          System.out.println(myDouble);
+          System.out.println(myint);
+       */
+          
+       char c1 = 1;
+       char c2 = 2;
+       int a = c2;
+       int b = c1;
+       System.out.println(a);
+       System.out.println(b);
+       
+       /*
+       int myint = 20;
+       byte myBytes = (byte) myint;
+       System.out.println(myBytes);
+       */
+    }
+}
